f0388c896bb2678d8f7adca33c834d7d7820417b
Scott Grant <scott.grant@thrasher.io>
2018-10-05 10:59:53 +1000

    Adds bithumb support for fee calculation

--- exchanges/bithumb/bithumb_wrapper.go
+++ exchanges/bithumb/bithumb_wrapper.go
@@ -188,3 +188,12 @@ func (b *Bithumb) WithdrawFiatExchangeFunds(currency pair.CurrencyItem, amount f
 func (b *Bithumb) WithdrawFiatExchangeFundsToInternationalBank(currency pair.CurrencyItem, amount float64) (string, error) {
 	return "", errors.New("not yet implemented")
 }
+
+// GetFeeByType returns an estimate of fee based on type of transaction
+func (b *Bithumb) GetFeeByType(feeType string, currencyPair string, purchasePrice float64, amount float64, isTaker bool, isMaker bool) (float64, error) {
+	resp, err := b.GetFee(feeType, currencyPair, purchasePrice, amount, isTaker, isMaker)
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
